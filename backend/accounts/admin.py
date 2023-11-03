ba69a69c6bea5a7a75cbb3953d4e0d70c779080b
chihab <chihab.mg.me@gmail.com>
2023-11-03 19:10:02 +0100

    create the custom user model

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_superuser",
+    )
+    list_filter = ("is_active","is_superuser")
+    search_fields = ("email", "email")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_staff", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff = True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser = True")
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField("email", unique=True)
+    first_name = models.CharField("first name", max_length=200, blank=True)
+    last_name = models.CharField("last name", max_length=200, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField("active", default=True)
+    is_staff = models.BooleanField("is staff", default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
