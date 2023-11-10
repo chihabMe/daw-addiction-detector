6632d92198f2fd762b65d7bb2ecbabe3f9fdff9f
chihab <chihab.mg.me@gmail.com>
2023-11-10 11:19:29 +0100

    changed the toaster success icon

--- frontend/src/context/ui.context.tsx
+++ frontend/src/context/ui.context.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 type IQuestionType = "list" | "grid";
 type ITheme = "dark"|"light";
 interface UiContextInitialState {
@@ -26,18 +27,23 @@ export const UiContextProvider = ({children}:{children:ReactNode})=>{
   const [theme,setTheme] = useState(initialState.theme)
   const [questionMode,setQuestionMode] = useState(initialState.questionMode)
 
-  const changeThemeToDark = ()=>setTheme("dark")
-  const changeThemeToLight = ()=>setTheme("light")
-  const changeQuestionModeToGrid=()=>setQuestionMode("grid")
-  const changeQuestionModeToList=()=>setQuestionMode("list")
+  const changeThemeToDark = ()=>{
+    toast.success("dark theme on")
+  setTheme("dark")
+  }
+  const changeThemeToLight = ()=>{
+    toast.success("light theme on")
+  setTheme("light")
+  }
+  const changeQuestionModeToGrid=()=>{
+    toast.success("grid display")
+  setQuestionMode("grid")
+  }
+  const changeQuestionModeToList=()=>{
+    toast.success("list display")
+  setQuestionMode("list")
+  }
 
-  // useEffect(()=>{
-  //     const questionModeFromLocalStorage = localStorage.getItem("questionMode")
-  //   console.log(questionMode)
-  //     if(questionModeFromLocalStorage)
-  //       setQuestionMode(questionModeFromLocalStorage as IQuestionType)
-  //
-  // },[])
 
   useEffect(()=>{
     localStorage.setItem("questionMode",questionMode)
--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -2,13 +2,14 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/layout/Header/Header";
 import Footer from "../components/layout/Footer/Footer";
 import { Toaster } from "react-hot-toast";
+import {CheckCircleIcon} from "@heroicons/react/24/solid"
 export function Root() {
   return (
     <>
       <Header />
       <Outlet />
       <Footer/>
-      <Toaster   position="bottom-right"   toastOptions={{duration:4500,className:"bg-white  min-w-[200px] font-medium"}}  />
+      <Toaster   position="bottom-right"   toastOptions={{duration:4500,className:"bg-white  min-w-[200px] font-medium",icon:<CheckCircleIcon className="w-6 h-6 text-blue-500"/>}}  />
     </> 
   );
 }
