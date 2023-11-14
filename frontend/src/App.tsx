a9c4a2dfd78d884d04790864e15dcd19e5b74405
chihab <chihab.mg.me@gmail.com>
2023-11-14 11:16:33 +0100

    changed the dark theme animaiton type

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,9 +10,9 @@ function App() {
         {theme=="dark" && (
           <motion.div
             key="dark_theme_overlay"
-            transition={{delay:0.1}}
-            initial={{top:0,right:0,opacity:0,radius:"100%"}}
-            exit={{top:0,right:0,opacity:0,radius:"100%"}}
+            transition={{}}
+            initial={{opacity:0}}
+            exit={{opacity:1}}
             animate={{top:0,right:0,bottom:0,left:0,opacity:1,radius:0}}
             className="-z-10 fixed      bg-light  dark:bg-dark"></motion.div>
         )}
