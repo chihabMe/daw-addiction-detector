ff18413490971b4907e2634327fc2e6ac47fd09f
chihab <chihab.mg.me@gmail.com>
2023-11-17 20:36:58 +0100

    added exit animatios to the scrolling header component

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -27,18 +27,18 @@ const links = [
 
 const Header = () => {
   const [isScrolling, setIsScrolling] = useState(false);
-  const prevYValue = useRef(0)
+  const prevYValue = useRef(0);
   const handleScroll = () => {
     console.log("y:", window.scrollY);
     console.log("height:", window.innerHeight);
 
     // if (window.scrollY >= window.innerHeight - 200) {
-     if(window.scrollY>prevYValue.current+50){
+    if (window.scrollY > prevYValue.current + 50) {
       setIsScrolling(true);
-    } else if(window.scrollY<prevYValue.current-50) {
+    } else if (window.scrollY < prevYValue.current - 50) {
       setIsScrolling(false);
     }
-    prevYValue.current=window.scrollY;
+    prevYValue.current = window.scrollY;
   };
   useEffect(() => {
     if (window) {
@@ -48,35 +48,46 @@ const Header = () => {
   }, []);
   console.log(isScrolling);
   return (
-    <AnimatePresence >
-      {isScrolling ? <ScrollingHeader /> : <DefaultHeader />}
-    </AnimatePresence>
+    <>
+      <AnimatePresence>{isScrolling && <ScrollingHeader />}</AnimatePresence>
+      <AnimatePresence>{!isScrolling && <DefaultHeader />}</AnimatePresence>
+    </>
   );
 };
 const ScrollingHeader = () => {
   const pathname = useLocation().pathname;
   return (
     <motion.header
-      initial={{ y: "-50%", opacity: 0,scale:0.8 }}
-      animate={{ y: 0, opacity: 1,scale:1 }}
-      exit={{ y: "-50%", opacity: 0,scale:0 ,transition:{duration:0.5}}}
-
-      transition={{duration:0.2}}
+      initial={{ y: "-50%", opacity: 0, scale: 0.8 }}
+      animate={{ y: 0, opacity: 1, scale: 1 }}
+      exit={{ y: -100, opacity: 0,scale:0.9}}
+      transition={{ duration: 0.3 }}
       className="w-full sticky top-4 z-50  "
     >
       <ul className="flex  relative   gap-2 w-full mx-auto py-4 rounded-xl items-center justify-around px-4  max-w-[600px]     ">
         <div className="  rounded-full z-40  absolute bg-gradient-to-r from-cyan-500 to-blue-500 top-0 bottom-0 left-0 right-0 " />
-        {links.map((link) => (
+        {links.map((link, idx) => (
           <Link key={link.href} to={link.href}>
-            <li className="  z-50 capitalize cursor-pointer px-6 py-3  font-medium relative ">
+            <motion.li
+              initial={{ y: -20 }}
+              animate={{ y: 0 }}
+              transition={{ delay: 0.1 * idx }}
+              className="  z-50 capitalize cursor-pointer px-6 py-3  font-medium relative "
+            >
               {link.href == pathname && (
                 <motion.div
                   layoutId="mini_nav_item_background"
                   className="bg-white rounded-full   absolute top-0 bottom-0 right-0 left-0"
                 ></motion.div>
               )}
-              <span className={ `relative ${link.href==pathname&&"!text-primary"} text-white hover:text-gray-200 ` }>{link.text}</span>
-            </li>
+              <span
+                className={`relative ${
+                  link.href == pathname && "!text-primary"
+                } text-white hover:text-gray-200 `}
+              >
+                {link.text}
+              </span>
+            </motion.li>
           </Link>
         ))}
       </ul>
