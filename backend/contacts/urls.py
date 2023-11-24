02523b8570caad544f119a7cc369c4f1a7a721c1
chihab <chihab.mg.me@gmail.com>
2023-11-24 22:13:41 +0100

    improved the contact_list_save view

--- backend/contacts/urls.py
+++ backend/contacts/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 from . import views
 app_name="contacts"
 urlpatterns = [
-   path("",views.contact_list_save)
+   path("",views.contact_list_save),
 ]
--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAdminUser
 from .models import Contact
 from rest_framework.decorators import api_view
 from .serializer import ContactSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 # Create your views here.
 
 @api_view(['GET','POST'])
 def contact_list_save(request):
-    contact= Contact.objects.all()
+    contact= Contact.objects.all()#
     if request.method == "POST":
-        serializer=ContactSerializer(data=request.data)
+        serializer=ContactSerializer(data=request.data)#
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    else:
-    
-        serializer = ContactSerializer(contact, many= True)
-        return Response(serializer.data)
+    serializer = ContactSerializer(contact, many= True)
+    return Response(serializer.data)
+
 
 
 
