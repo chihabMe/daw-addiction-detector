e3402afb7eb628609edcf7ec1062893c928e0da1
chihab <chihab.mg.me@gmail.com>
2023-11-24 17:58:11 +0100

    added custom axios isntance with refresh token

---
+++ frontend/src/utils/axios_client.ts
@@ -0,0 +1,62 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { API_URL, refreshtokenPath } from "./constants";
+import { jwtDecode } from "jwt-decode";
+
+interface DecodedToken {
+  exp: number;
+  sub: string;
+  // Add other fields based on your JWT structure
+}
+
+let accessToken = localStorage.getItem("access") ?? null;
+
+export const axiosClient = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${accessToken}`,
+  },
+});
+
+axiosClient.interceptors.request.use(async (req) => {
+  if (!accessToken) {
+    accessToken = localStorage.getItem("access") ?? null;
+  }
+
+  if (accessToken) {
+    const user: DecodedToken = jwtDecode(accessToken);
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    const isValid = user && user.exp > currentTimestamp;
+
+    if (isValid) {
+      req.headers.Authorization = `Bearer ${accessToken}`;
+      return req;
+    }
+
+    const refreshToken = localStorage.getItem("refresh");
+
+    if (!refreshToken) {
+      // If there's no refresh token, simply return the request
+      return req;
+    }
+
+    try {
+      // Attempt to refresh the access token using the refresh token
+      const response = await axios.post(refreshtokenPath, {
+        refresh: refreshToken,
+      });
+
+      // Update the access token in local storage
+      localStorage.setItem("access", response.data.access);
+
+      // Update the Authorization header with the new access token
+      req.headers.Authorization = `Bearer ${response.data.access}`;
+    } catch (error) {
+      // Handle the error (e.g., log it, redirect to login, etc.)
+      console.error("Token refresh failed:", error);
+    }
+  }
+
+  return req;
+});
+export default axiosClient;
--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,12 +1,10 @@
+export const API_URL = import.meta.env.VITE_BACKEND_URL;
 
-const API_URL = import.meta.env.VITE_BACKEND_URL;
+export const getAllQuestionsPath = API_URL + "/api/v1/questions/";
+export const getAllQuestionResponseOptionsPath =
+  API_URL + "/api/v1/questions/options/";
 
-export const getAllQuestionsPath = API_URL+"/api/v1/questions/"
-export const getAllQuestionResponseOptionsPath = API_URL+"/api/v1/questions/options/"
-
-export const obtainTokenPath = API_URL+"/api/v1/auth/token/obtain"
-export const refreshtokenPath = API_URL+"/api/v1/auth/token/refresh/"
-export const veriftyTokenPath = API_URL+"/api/v1/auth/token/veirfy/"
-export const getUserPath = API_URL+"/api/v1/auth/me"
-
-
+export const obtainTokenPath = API_URL + "/api/v1/auth/token/obtain";
+export const refreshtokenPath = "/api/v1/auth/token/refresh/";
+export const veriftyTokenPath = API_URL + "/api/v1/auth/token/veirfy/";
+export const getUserPath = API_URL + "/api/v1/auth/me";
