cc5ec84529d7e9f6205d7854dcf72c5cd0a78982
see4th <anesbenmeddour@gmail.com>
2023-11-27 22:53:05 +0100

    fixed the validator errur

--- backend/contacts/serializer.py
+++ backend/contacts/serializer.py
@@ -5,11 +5,14 @@ class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = ["email","first_name","last_name","text","send_date", "phone_number"]
-        def validate_phone_number(self,phone_number):
-            for c in"AZERTYUIOPQSDFGJKLWXCVBNNLKlM":
-                if c in phone_number:
-                    return serializers.ValidationError("invalid phone number!")
+    def validate_phone_number(self,phone_number):
+        txt = "AZERTYUIOPQSDFGJKLWXCVBNNLKlM"
+        print("hifuc")
+        for c in txt  :
+            if c in phone_number or c in txt.upper() or c in txt.lower():
+
+                raise serializers.ValidationError("invalid phone number!")
             
-            return phone_number
+        return phone_number
 
     
