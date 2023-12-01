ba11a4948da22346719814ed145a045351b8c34f
chihab <chihab.mg.me@gmail.com>
2023-12-01 17:43:57 +0100

    added mysql config for the production env

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("MODE") == "PRODUCTION"
-DEBUG = True
+DEBUG = os.environ.get("MODE") != "PRODUCTION"
 
 ALLOWED_HOSTS = [
     "localhost:8000",
@@ -109,13 +108,24 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.environ.get("DB_NAME"),
+            "USER":os.environ.get("DB_USER"),
+            "PASSWORD":os.environ.get("DB_PASSWORD"),
+            "HOST":os.environ.get("DB_HOST"),
+            "PORT":os.environ.get("DB_PORT"),
+        }
+    }
 
 
 # Password validation
