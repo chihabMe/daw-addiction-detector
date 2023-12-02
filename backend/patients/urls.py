afc9f0a60b7e96274a98c18b10d50c7f70c06f83
chihab <chihab.mg.me@gmail.com>
2023-12-02 11:00:05 +0100

    removed unused PatientListView

--- backend/patients/urls.py
+++ backend/patients/urls.py
@@ -6,5 +6,4 @@ app_name = "patients"
 
 urlpatterns = [
     path("", views.patients_list, name="patients_list"),
-    path("v2/", views.PatientListCreateView.as_view(), name="patients_list_create"),
 ]
--- backend/patients/views.py
+++ backend/patients/views.py
@@ -17,19 +17,10 @@ def patients_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # def create(self, request, *args, **kwargs):
-    # serializer = self.get_serializer(data=request.data)
-    # serializer.is_valid(raise_exception=True)
-    # self.perform_create(serializer)
-    # headers = self.get_success_headers(serializer.data)
-    # return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-
 class PatientListCreateView(ListCreateAPIView):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
 
-    # permission_classes=[IsAuthenticated]
     def create(self, request, *args, **kwargs):
         user = request.user
         return super().create(request, *args, **kwargs)
