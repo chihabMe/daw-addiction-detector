e7da307341cd8c70bdaee4530cb0ff3497811044
chihab <chihab.mg.me@gmail.com>
2023-12-02 10:44:37 +0100

    created api_v1 list to imporve the  struction of  the apis verions

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,21 +1,25 @@
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    path("admin/", include("admin_volt.urls")),
-    path("admin/", admin.site.urls),
+api_v1 = [
     path("api/v1/patients/", include("patients.urls", namespace="patients")),
     path("api/v1/questions/", include("questions.urls", namespace="questions")),
-    path("api/v1/answers/",include("answers.urls",namespace="answers")),
+    path("api/v1/answers/", include("answers.urls", namespace="answers")),
     path("api/v1/auth/", include("authentication.urls", namespace="authentication")),
     path("api/v1/contact", include("contacts.urls", namespace="contacts")),
     path("api/v1/accounts", include("accounts.urls", namespace="accounts")),
     path("api/v1/reviews/", include("reviews.urls", namespace="reviews")),
-    path("api/v1/uisettings/",include("uisettings.urls",namespace="uisettings")),
+    path("api/v1/uisettings/", include("uisettings.urls", namespace="uisettings")),
 ]
+api_v2 = []
+
+urlpatterns = [
+    path("admin/", include("admin_volt.urls")),
+    path("admin/", admin.site.urls),
+]
+urlpatterns += api_v1
 
 
 if settings.DEBUG:
