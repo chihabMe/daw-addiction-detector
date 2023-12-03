10051065b8b86c979d43bf515a7c2c299b73c1f6
chihab <chihab.mg.me@gmail.com>
2023-12-03 23:32:03 +0100

    added the ability to show the profile image in the profile page

--- frontend/src/components/pages/profile/ProfieImageInfos.tsx
+++ frontend/src/components/pages/profile/ProfieImageInfos.tsx
@@ -7,7 +7,7 @@ const ProfileImageInfos = (props: Props) => {
     <div className="outline-1 outline w-full outline-gray-200   rounded-md px-4 py-4 flex items-start justify-between ">
       <div className="flex gap-4 ">
         <div>
-          <img className="w-[110px] h-[110px] rounded-full bg-blue-400" />
+          <img alt={`${props.first_name} profle image`} src={props.image} className="w-[110px] h-[110px] rounded-full " />
         </div>
         <div className="flex  flex-col ">
           <h2 className=" text-gray-700 py-1 font-bold capitalize dark:text-text-lighter">
@@ -22,10 +22,8 @@ const ProfileImageInfos = (props: Props) => {
       </div>
       <div className="">
         <Button className="bg-transparent flex items-center gap-2 px-2 py-2 ">
-          <PencilIcon className="w-3 h-3 text-gray-400"/>
-          <span className="text-gray-400 text-sm">
-            Edite
-          </span>
+          <PencilIcon className="w-3 h-3 text-gray-400" />
+          <span className="text-gray-400 text-sm">Edite</span>
         </Button>
       </div>
     </div>
