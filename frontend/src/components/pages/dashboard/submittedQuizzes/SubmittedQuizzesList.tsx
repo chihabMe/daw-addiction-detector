c649ab73c9adec07db275d23dd056fd6e52f862a
chihabMe <chihab.mg.me@gmail.com>
2024-01-06 16:38:46 +0100

    completed the ui for the submission deatials page

--- frontend/src/components/pages/dashboard/submittedQuizzes/SubmittedQuizzesList.tsx
+++ frontend/src/components/pages/dashboard/submittedQuizzes/SubmittedQuizzesList.tsx
@@ -1,18 +1,44 @@
 import { useAuth } from "../../../../hooks/useAuth";
 import SubmittedQuizItem from "./SubmittedQuizItem";
+import { motion } from "framer-motion";
 
 const SubmittedQuizzesList = () => {
   const { user } = useAuth();
   if (!user) return <span>error</span>;
+  const submissions: { user: IUser; quiz: { id: string } }[] = [
+    {
+      user,
+      quiz: { id: "hisd" },
+    },
+    {
+      user,
+      quiz: { id: "hisd" },
+    },
+    {
+      user,
+      quiz: { id: "hisd" },
+    },
+    {
+      user,
+      quiz: { id: "hisd" },
+    },
+    {
+      user,
+      quiz: { id: "hisd" },
+    },
+  ];
   return (
     <div className="max-w-screen-xl pt-10 flex justify-between  gap-2 ">
       <ul className="w-full  max-w-[800px]      gap-2 flex flex-col ">
-        <SubmittedQuizItem user={user} quiz={{id:"hi"}} />
-        <SubmittedQuizItem user={user} quiz={{id:"hi"}} />
-        <SubmittedQuizItem user={user} quiz={{id:"hi"}} />
-        <SubmittedQuizItem user={user} quiz={{id:"hi"}} />
-        <SubmittedQuizItem user={user} quiz={{id:"hi"}} />
-        <SubmittedQuizItem user={user} quiz={{id:"hi"}} />
+        {submissions.map((item, idx) => (
+          <motion.div
+            initial={{ y: -40, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: idx * 0.1, duration: 0.3 }}
+          >
+            <SubmittedQuizItem user={item.user} quiz={item.quiz} />
+          </motion.div>
+        ))}
       </ul>
       <div className="bg-blue-50 rounded-lg max-w-[280px] flex-[20%]">
         <h1>Filters</h1>
--- frontend/src/pages/dashboard/submitted/submittedQuizDetails.tsx
+++ frontend/src/pages/dashboard/submitted/submittedQuizDetails.tsx
@@ -1,7 +1,54 @@
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import { Link, useParams } from "react-router-dom";
 import Button from "../../../components/ui/Button";
-
+import { AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
+interface SubmittedItem {
+  quiz: string;
+  response: string;
+}
+const submissions: SubmittedItem[] = [
+  {
+    quiz: "how many hours do you play",
+    response: "4 hours a day",
+  },
+  {
+    quiz: "how many hours do you play",
+    response: "4 hours a day",
+  },
+  {
+    quiz: "how many hours do you play",
+    response: "4 hours a day",
+  },
+  {
+    quiz: "how many hours do you play",
+    response: "4 hours a day",
+  },
+  {
+    quiz: "how many hours do you play",
+    response: "4 hours a day",
+  },
+  {
+    quiz: "how many hours do you play",
+    response: "4 hours a day",
+  },
+  {
+    quiz: "how many hours do you play",
+    response: "4 hours a day",
+  },
+  {
+    quiz: "how many hours do you play",
+    response: "4 hours a day",
+  },
+  {
+    quiz: "how many hours do you play",
+    response: "4 hours a day",
+  },
+  {
+    quiz: "how many hours do you play",
+    response: "4 hours a day",
+  },
+];
 const SubmittedQuizDetails = () => {
   const { submissionId } = useParams();
   return (
@@ -13,8 +60,46 @@ const SubmittedQuizDetails = () => {
           </Button>
         </Link>
       </section>
-      <section>{submissionId}</section>
+      <section className="pt-10 flex justify-between w-full max-w-screen-xl">
+        <div className=" w-full max-w-screen-sm">
+          <ul className="flex flex-col px-4 gap-4 h-[800px] overflow-y-scroll scroll-hide">
+            {submissions.map((item, idx) => (
+              <AnimatePresence>
+                <motion.div
+                  initial={{ y: -40, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ delay: idx * 0.1, duration: 0.5 }}
+                >
+                  <SubmittedQuizItem
+                    quiz={item.quiz}
+                    response={item.response}
+                  />
+                </motion.div>
+              </AnimatePresence>
+            ))}
+          </ul>
+        </div>
+        <div className="bg-blue-50 flex-[20%] max-w-[400px] rounded-lg">
+          <h1>review</h1>
+        </div>
+      </section>
     </main>
+  );
+};
+const SubmittedQuizItem = (props: SubmittedItem) => {
+  return (
+    <li className="flex flex-col gap-3">
+      <span className="rounded-lg p-4 bg-light dark:bg-dark ">
+        {props.quiz}
+      </span>
+      <span className="rounded-lg p-4 bg-blue-400 text-white flex gap-2 items-center">
+        <div
+          className={` 
+               group-hover:bg-white bg-white group-active:animate-ping    rounded-full w-3 h-3  `}
+        ></div>
+        {props.response}
+      </span>
+    </li>
   );
 };
 
